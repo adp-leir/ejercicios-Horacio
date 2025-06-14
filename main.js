afaea66671f5cfb69e5f244432e299da4c966784
adp-leir <arnau.dejuan1@gmail.com>
2025-06-14 11:26:11 +0000

    arnau

---
+++ main.js
@@ -0,0 +1,12 @@
+//Crear una función que tome por parámetros una palabra y convierta las letras de esa palabra a mayusculas
+
+function convertirpalabra (palabra){
+   let resultado = palabra.toUpperCase();
+   return resultado;
+}
+let final = convertirpalabra("barcelona");
+console.log(final);
+
+
+
+
